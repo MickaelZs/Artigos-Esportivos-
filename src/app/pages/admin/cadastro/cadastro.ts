6d1ba03a4fc79df0b9d9c34a7bd72ffc7514065e
Mickaelzs <mickaelcosta431@gmail.com>
2025-09-15 16:55:25 -0300

    form produtos component

--- src/app/pages/admin/cadastro/cadastro.ts
+++ src/app/pages/admin/cadastro/cadastro.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderAdmin } from "../../../shared/header-admin/header-admin";
+import { FormProdutos } from "../../../shared/form-produtos/form-produtos";
 
 
 @Component({
   selector: 'app-cadastro',
-  imports: [HeaderAdmin],
+  imports: [HeaderAdmin, FormProdutos],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css'
 })
---
+++ src/app/shared/form-produtos/form-produtos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-produtos',
+  imports: [],
+  templateUrl: './form-produtos.html',
+  styleUrl: './form-produtos.css'
+})
+export class FormProdutos {
+
+}
