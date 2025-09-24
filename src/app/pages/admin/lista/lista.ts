3bf0b637718675a321d8585a30fff95d88338b75
Mickaelzs <mickaelcosta431@gmail.com>
2025-09-24 19:16:58 -0300

    create compont listagem de produtos

--- src/app/pages/admin/lista/lista.ts
+++ src/app/pages/admin/lista/lista.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderAdmin } from "../../../shared/header-admin/header-admin";
 import { InputBuscar } from "../../../shared/input-buscar/input-buscar";
+import { DetalhesProdutos } from "../../detalhes-produtos/detalhes-produtos";
+import { Faixa } from "../../../shared/faixa/faixa";
+import { TabelaListagem } from "../../../shared/tabela-listagem/tabela-listagem";
+import { FormProdutos } from "../../../shared/form-produtos/form-produtos";
+import { CardProdutos } from "../../../shared/card-produtos/card-produtos";
 
 
 @Component({
   selector: 'app-lista',
-  imports: [HeaderAdmin, InputBuscar],
+  imports: [HeaderAdmin, InputBuscar, Faixa, TabelaListagem],
   templateUrl: './lista.html',
   styleUrl: './lista.css'
 })
---
+++ src/app/shared/tabela-listagem/tabela-listagem.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabela-listagem',
+  imports: [],
+  templateUrl: './tabela-listagem.html',
+  styleUrl: './tabela-listagem.css'
+})
+export class TabelaListagem {
+
+}
