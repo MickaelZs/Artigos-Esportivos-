a81f0b6e08f112f4864ef492b35ed32a853d8eb1
Mickaelzs <mickaelcosta431@gmail.com>
2025-10-23 22:26:39 -0300

    add list product

--- src/app/shared/card-produtos/card-produtos.ts
+++ src/app/shared/card-produtos/card-produtos.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { ProdutosService } from '../../services/produtos';
+import { Produtos } from '../../services/types/type';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card-produtos',
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule],
   templateUrl: './card-produtos.html',
   styleUrl: './card-produtos.css'
 })
-export class CardProdutos {
+export class CardProdutos implements OnInit {
+
+  listaProdutos: Produtos[] = [];
+    constructor(
+      private service: ProdutosService,
+      private router: Router,
+      private cdr: ChangeDetectorRef
+    ) { }
+  
+    ngOnInit(): void {
+      this.service.listar().subscribe({
+        next: (produtos) => {
+          this.listaProdutos = produtos;
+          this.cdr.detectChanges();
+        }
+      });
+    }
+
+    trackById(index: number, produto: Produtos): string {
+        return produto.id ? produto.id.toString() : index.toString();
+      }
+    
 
 }
